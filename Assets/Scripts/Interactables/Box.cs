62405dd2bccba762bc08251f3a27dead2ef873ab
Tiago Pereira <tiagomaiapereira900@gmail.com>
2022-05-03 17:01:10 +0100

    Alien Box Done
    Finished the Alien Box Script

--- Assets/Scripts/Interactables/Box.cs
+++ Assets/Scripts/Interactables/Box.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Box : MonoBehaviour
+public class Box : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer spriteRenderer = null;
+
+    [SerializeField]
+    private Sprite[] sprites = null;
+
+    [SerializeField]
+    private GameObject lightBeam = null;
+
+    private bool boxOff = true;
+
+    private void Awake()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Interaction>().SetInteractable(this);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Interaction>().SetInteractable(null);
+        }
+    }
+
+    public void Interact()
+    {
+        if (boxOff)
+        {
+            EnableBeam();
+        }
+        else
+        {
+            DisableBeam();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EnableBeam()
     {
-        
+        lightBeam.SetActive(true);
+        spriteRenderer.sprite = sprites[1];
+        boxOff = false;
+    }
+
+    private void DisableBeam()
+    {
+        lightBeam.SetActive(false);
+        spriteRenderer.sprite = sprites[0];
+        boxOff = true;
     }
 }
